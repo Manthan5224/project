ccf2ea845751b647f61aca7401226007acfb0fbe
Manthan <72142658+Manthan5224@users.noreply.github.com>
2021-07-11 11:43:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+// var i = 0;
+// var images = [];
+// var slideTime = 1000; // 3 seconds
+// const dl = document.getElementsByClassName('.top')
+
+// images[0] = 'images/flower.jpg';
+// images[1] = 'images/castor.jpg';
+// images[2] = 'https://via.placeholder.com/150/AAA300';
+
+// function changePicture() {
+//     document.dl.src = images[i];
+
+//     if (i < images.length - 1) {
+//         i++;
+//     } else {
+//         i = 0;
+//     }
+//     setTimeout(changePicture(), slideTime);
+// }
+
+// window.onload = changePicture;
+
+
+
+//-------------------------------------------------------
+
+// <!-- <script>
+//     var i = 0;
+//     var images = [];
+//     var slideTime = 3000; // 3 seconds
+//     var dl = document.getElementById('#top');
+    
+//     images[0] = 'images/flower.jpg';
+//     images[1] = 'images/castor.jpg';
+//     images[2] = 'images/Head.jpg';
+    
+//     function changeImg() {
+//         document.top.src = images[i];
+    
+//         if (i < images.length - 1) {
+//             i++;
+//         } else {
+//             i = 0;
+//         }
+//         setTimeout("changeImg()", slideTime);
+//     }
+    
+//     window.onload = changeImg;
+//     </script> -->
+//     <!-- <script>
+//         var slideIndex = 0;
+//         showSlides();
+        
+//         function showSlides() {
+//           var i;
+//           var slides = document.getElementsByClassName("top");
+//           var dots = document.getElementsByClassName("dot");
+//           for (i = 0; i < slides.length; i++) {
+//             slides[i].style.display = "none";  
+//           }
+//           slideIndex++;
+//           if (slideIndex > slides.length) {slideIndex = 1}    
+//           for (i = 0; i < dots.length; i++) {
+//             dots[i].className = dots[i].className.replace(" active", "");
+//           }
+//           slides[slideIndex-1].style.display = "block";  
+//           dots[slideIndex-1].className += " active";
+//           setTimeout(showSlides, 2000); // Change image every 2 seconds
+//         }
+//         </script> -->
